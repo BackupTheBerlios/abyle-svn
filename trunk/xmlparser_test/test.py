9c8f8887156142387fc7407c357a88cb8a4d94cc
scuq <scuq@74900e50-a41e-0410-99d4-a0bbae8d93cc>
2007-06-13 23:07:23 +0000

    building new xml parser ....

--- trunk/xmlparser_test/test.py
+++ trunk/xmlparser_test/test.py
@@ -2,10 +2,6 @@ from xml import xpath
 from xml.dom.minidom import *
 doc = parse ("./iptables_flags.xml").documentElement
 #traffic_nodes = xpath.Evaluate('/flags/flag[@index="2"]/@cli_arg', doc)
-traffic_nodes = xpath.Evaluate('/flags/flag[flag="destination"]/@cli_arg', doc)
+traffic_nodes = xpath.Evaluate('/flags/flag[text()="destination"]/@cli_arg', doc)
 
-print traffic_nodes
-cnt=0
-for node in traffic_nodes:
-	
-	print node.value
+print traffic_nodes[0].value
