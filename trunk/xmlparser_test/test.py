2d4ea90b1a61192adeceff31ab3bf21b110a760d
scuq <scuq@74900e50-a41e-0410-99d4-a0bbae8d93cc>
2007-07-31 08:33:58 +0000

    fixed iptables_flaags.xml recent bug

--- trunk/xmlparser_test/test.py
+++
@@ -1,7 +0,0 @@
-from xml import xpath
-from xml.dom.minidom import *
-doc = parse ("./iptables_flags.xml").documentElement
-#traffic_nodes = xpath.Evaluate('/flags/flag[@index="2"]/@cli_arg', doc)
-traffic_nodes = xpath.Evaluate('/flags/flag[text()="destination"]/@cli_arg', doc)
-
-print traffic_nodes[0].value
